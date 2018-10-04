7f3daf80c15745b3d12fad9beef636acc27886cb
Ziggy Vergne <ziggy.vergne@gmail.com>
2018-10-04 21:48:54 +0200

    Add voc and pl, remove output to file (can use redirection of STDOUT)

--- analysis.py
+++ analysis.py
@@ -5,37 +5,63 @@ from nltk import tokenize
 from nltk.stem import PorterStemmer
 from nltk.tokenize import RegexpTokenizer
 from pathlib import Path
+from nltk.corpus import stopwords
 
-from pathlib import Path
-
+voc = dict()
+#todo : merge inverted file among several docs
+#handle parametrisation of estimation of score (instead of just incrementing)
+#store it and access it in an optimized way on/from HDD
+#add test for tokenizer
+#add perf evaluation
+#add more functions inside
+#structure & comment code
+#have fun
 def analyseNewspaper(path):
 
     contents = path.read_text()
     ps = PorterStemmer()    
     tree = etree.fromstring("<NEWSPAPER>"+contents+"</NEWSPAPER>")
-    article = ""
+    text = ""
     regexMoney = re.compile(r"(\$|€|¥)\d+((\.\d+)?(\s(million|billion))?)?")
     regexNumber = re.compile(r"\d+((\.\d+)?(\s(million|billion))?)?")
-    f = open("parsingResult.txt", "w")
-    f.close()
-    for text in tree.xpath("//DOC/TEXT"):
-        article = ""
-        for p in text.xpath("./P"):
-            article += p.text
-        article = re.sub(regexMoney, "<money>", article)
-        article = re.sub(regexNumber,"<number>",article)
+    stop_words = [word.lower() for word in set(stopwords.words('english')) ]
+    for document in tree.xpath("//DOC"):
+        text = ""
+        vocDoc = {}
+        idDocument = document.xpath("./DOCID")[0].text
+        numDocument = document.xpath("./DOCNO")[0].text
+        for p in document.xpath("./TEXT/P"):
+            text += p.text
+        text = re.sub(regexMoney, "<money>", text)
+        text = re.sub(regexNumber,"<number>", text)
 
         tokenizer = RegexpTokenizer(r'([\w\-\<\>]+)')
-        listofwords = tokenizer.tokenize(article)
+        listofwords = tokenizer.tokenize(text)
         f = open("parsingResult.txt", "a")
         for i,word in enumerate(listofwords):
-            listofwords[i] = ps.stem(word)
-            f.write(listofwords[i] + ';')
-        # print(listofwords)
-        f.close()
+            if word.lower() not in stop_words:
+                word = ps.stem(word)
+                word = word.lower()
+                if word in vocDoc:
+                    vocDoc[word] = vocDoc[word] + 1
+                else:
+                    vocDoc[word] = 1
+        for word, occ in vocDoc.items():
+            if word in voc:
+                voc[word][numDocument] = occ #occurencies
+            else:
+                voc[word] = {}
+                voc[word][numDocument] = occ #occurencies
 
-# TODO: Change the pathlist to latimes to parse all the files
-# pathlist = Path("data/latimes/").glob('**/la*')
+        f.close()
+    #print the aggregation
+    for word, pl in voc.items():
+        print(word)
+        for numDoc, score in pl.items():
+            print("{} => {}.".format(numDoc, score))
+
+
+# todo : add parametrization from command line to choose which folder we shoud parse
 pathlist = Path("data/latimesMini/").glob('**/la*')
 
 i = 1
