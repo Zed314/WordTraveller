922aa59b3b4c84b3204d23a6716ab5ac75ef3e4f
Alex Huang Feng <alexhuang05@gmail.com>
2018-10-23 23:43:22 +0200

    refactor

--- wordtraveller/faginstopk.py
+++ wordtraveller/faginstopk.py
@@ -9,16 +9,7 @@ from pathlib import Path
 last_score_of_c = 0
 
 
-def createMockData(ownfilemanager):
-    voc = SortedDict()
-
-    pathlist = Path("./tests/data/test1/").glob('**/la*')
-    for path in pathlist:
-        analysis.analyse_newspaper(path, voc)
-    ownfilemanager.save_vocabularyAndPL_file(voc)
-
-
-def mean(values):
+def aggregative_function_mean(values):
     sumValues = sum(values)
     return sumValues/len(values)
 
@@ -29,7 +20,7 @@ def push_to_m(m, c, docId, score, nb_of_PL):
         # print('[{}],[{}],[{}],[{}]'.format(m[docId],nb_of_PL,len(m[docId]),docId))
         m[docId] += [score]
         if(len(m[docId]) == nb_of_PL):
-            mean_score = mean(m[docId])
+            mean_score = aggregative_function_mean(m[docId])
             # print('calculMean: {}|{}|{}'.format(docId,m[docId],mean_score))
             c[docId] = mean_score
             last_score_of_c = mean_score
@@ -56,49 +47,14 @@ def get_score_by_doc_id(doc_id, postingListsOrderedById, aggregation_function):
     score = aggregation_function(all_scores)
     return score
 
-def fagins_top_k_algo(words, voc, filemanager, k):
-    pl1_score = dict()
-    pl1_score[0.90] = [2]
-    # pl1_score[0.90] += [3]
-    pl1_score[0.80] = [5]
-    pl1_score[0.70] = [6]
-    pl1_score[0.60] = [4]
-    pl1_score[0.50] = [1]
-    pl1_score[0.40] = [3]
 
-    pl2_score = dict()
-    pl2_score[0.85] = [3]
-    pl2_score[0.80] = [5]
-    pl2_score[0.75] = [2]
-    pl2_score[0.74] = [6]
-    pl2_score[0.74] += [1]
-    pl2_score[0.70] = [4]
-
-    postingListsOrderedByScore = dict()
-    postingListsOrderedByScore['aaa'] = pl1_score
-    postingListsOrderedByScore['bbb'] = pl2_score
-    print('postingListsOrderedByScore: {}'.format(postingListsOrderedByScore))
-
-    pl1_id = dict()
-    pl1_id[3] = 0.40
-    pl1_id[1] = 0.50
-
-    pl2_id = dict()
-    pl2_id[3] = 0.85
-    pl2_id[1] = 0.74
-
-    postingListsOrderedById = dict()
-    postingListsOrderedById['aaa'] = pl1_id
-    postingListsOrderedById['bbb'] = pl2_id
-    # postingListsOrderedById = [pl1_id,pl2_id]
-
-    # Algo commence ici
+def find_fagins_top_k(postingListsOrderedById, postingListsOrderedByScore, k):
     global last_score_of_c
+
     iterators = dict()
     currentScores = SortedDict()
     # posting_list_id sera le terme du posting_list
     for posting_list_id in postingListsOrderedByScore:
-        print("POST:{}".format(postingListsOrderedByScore[posting_list_id]))
         iterators[posting_list_id] = iter(
             postingListsOrderedByScore[posting_list_id])
         # next donne la clé
@@ -110,7 +66,7 @@ def fagins_top_k_algo(words, voc, filemanager, k):
     c = dict()
     m = dict()
     while len(c) < k:
-        print("Current {}".format(currentScores))
+        # print("Current {}".format(currentScores))
         item = currentScores.popitem()
         score = item[0]
         postingListId = item[1][0][1]
@@ -139,30 +95,74 @@ def fagins_top_k_algo(words, voc, filemanager, k):
         except StopIteration:
             print("No more values in postingLists")
 
-    print("last_  {}".format(last_score_of_c))
     for doc_id in m:
-        score = get_score_by_doc_id(doc_id, postingListsOrderedById, mean)
+        score = get_score_by_doc_id(
+            doc_id, postingListsOrderedById, aggregative_function_mean)
         if(score > last_score_of_c):
-            c.popitem()
+            # TODO: regarder s'il y a moyen de ne pas faire for loop
+            for c_value in c:
+                if(c[c_value] == last_score_of_c):
+                    del c[c_value]
+                    break
             c[doc_id] = score
             last_score_of_c = score
-    print('final top {} : {}'.format(k,c))
+    print('final top {} : {}'.format(k, c))
     return c
+
+
+def createMockData():
+    pl1_score = dict()
+    pl1_score[0.90] = [2]
+    # pl1_score[0.90] += [3]
+    pl1_score[0.80] = [5]
+    pl1_score[0.70] = [6]
+    pl1_score[0.60] = [4]
+    pl1_score[0.50] = [1]
+    pl1_score[0.40] = [3]
+
+    pl2_score = dict()
+    pl2_score[0.85] = [3]
+    pl2_score[0.80] = [5]
+    pl2_score[0.75] = [2]
+    pl2_score[0.74] = [6]
+    pl2_score[0.74] += [1]
+    pl2_score[0.70] = [4]
+
+    postingListsOrderedByScore = dict()
+    postingListsOrderedByScore['aaa'] = pl1_score
+    postingListsOrderedByScore['bbb'] = pl2_score
+
+    pl1_id = dict()
+    pl1_id[3] = 0.40
+    pl1_id[1] = 0.50
+
+    pl2_id = dict()
+    pl2_id[3] = 0.85
+    pl2_id[1] = 0.74
+
+    postingListsOrderedById = dict()
+    postingListsOrderedById['aaa'] = pl1_id
+    postingListsOrderedById['bbb'] = pl2_id
+
+    return postingListsOrderedById, postingListsOrderedByScore
 
 
 if __name__ == "__main__":
     currentWorkspace = './tests/workspace/test1/'
     filename = 'test1'
     filemanag = fm.FileManager(filename, currentWorkspace)
-    createMockData(filemanag)
+
+    voc = SortedDict()
+
+    pathlist = Path("./tests/data/test1/").glob('**/la*')
+    for path in pathlist:
+        analysis.analyse_newspaper(path, voc)
+    filemanag.save_vocabularyAndPL_file(voc)
+
+    # Applying Top K Algorithme
+    postingListsOrderedById, postingListsOrderedByScore = createMockData()
 
     savedVoc = filemanag.read_vocabulary()
 
-    top_k = fagins_top_k_algo(["aa", "bb"], savedVoc, filemanag, 3)
-    
-    # mot1 = query.get_posting_list(savedVoc,"aa", filemanag)
-    # mot2 = query.get_posting_list(savedVoc,"bb", filemanag)
-    # mot3 = query.get_posting_list(savedVoc,"cc", filemanag)
-    # print(mot1)
-    # print(mot2)
-    # print(mot3)
+    top_k = find_fagins_top_k(postingListsOrderedById,
+                              postingListsOrderedByScore, 3)
