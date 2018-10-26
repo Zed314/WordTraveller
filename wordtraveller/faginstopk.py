4a1ae18d548b2f2e855fd295d33f8b5983e01c96
Alex Huang Feng <alexhuang05@gmail.com>
2018-10-26 15:22:16 +0200

    bugs

--- wordtraveller/faginstopk.py
+++ wordtraveller/faginstopk.py
@@ -8,6 +8,25 @@ from pathlib import Path
 # Cette variable me sers pour apres savoir si les scores qui restent dans m sont plus grandes que celles déjà existantes
 last_score_of_c = 0
 
+def apply_top_k_algo(words, voc, filemanager, k):
+    posting_lists_ordered_by_id = {word: query.get_posting_list(voc, word, filemanager) for word in words}
+    print('ww: {}'.format(posting_lists_ordered_by_id))
+
+    pl_aa = dict()
+    pl_aa[3] = [1]
+    pl_aa[2] = [2]
+    pl_aa[1] = [3]
+    pl_bb = dict()
+    pl_bb[1] = [1,2]
+    pl_cc = dict()
+    pl_cc[1] = [3]
+
+    posting_lists_ordered_by_score = dict()
+    posting_lists_ordered_by_score['aa'] = pl_aa
+    posting_lists_ordered_by_score['bb'] = pl_bb
+    posting_lists_ordered_by_score['cc'] = pl_cc
+    
+    find_fagins_top_k(posting_lists_ordered_by_id,posting_lists_ordered_by_score,k)
 
 def aggregative_function_mean(values):
     """
@@ -94,22 +113,24 @@ def find_fagins_top_k(postingListsOrderedById, postingListsOrderedByScore, k):
     c = dict()
     m = dict()
     while len(c) < k:
-        # print("Current {}".format(currentScores))
-        item = currentScores.popitem()
-        score = item[0]
-        postingListId = item[1][0][1]
-        docId = item[1][0][0]
-        push_to_m(m, c, docId, score, len(postingListsOrderedByScore),aggregative_function_mean)
-        if(len(item[1]) > 1):
-            for doc in item[1]:
-                used_docId = item[1][doc][0]
-                pl_id = item[1][doc][1]
-                if(not (docId == used_docId and pl_id == postingListId)):
-                    if score not in currentScores:
-                        currentScores[score] = dict()
-                    currentScores[score][len(currentScores[score])] = [
-                        used_docId, pl_id]
-
+        print("Current {}".format(currentScores))
+        if(len(currentScores) > 0):
+            item = currentScores.popitem()
+            score = item[0]
+            postingListId = item[1][0][1]
+            docId = item[1][0][0]
+            push_to_m(m, c, docId, score, len(postingListsOrderedByScore),aggregative_function_mean)
+            if(len(item[1]) > 1):
+                for doc in item[1]:
+                    used_docId = item[1][doc][0]
+                    pl_id = item[1][doc][1]
+                    if(not (docId == used_docId and pl_id == postingListId)):
+                        if score not in currentScores:
+                            currentScores[score] = dict()
+                        currentScores[score][len(currentScores[score])] = [
+                            used_docId, pl_id]
+        else:
+            break
         # getting next new score and add it to currentScores
         try:
             newScore = next(iterators[postingListId])
@@ -177,18 +198,22 @@ if __name__ == "__main__":
     filename = 'test1'
     filemanag = fm.FileManager(filename, currentWorkspace)
 
-    voc = SortedDict()
+    tempVoc = SortedDict()
 
     pathlist = Path("./tests/data/test1/").glob('**/la*')
     for path in pathlist:
-        print(path)
-        analysis.analyse_newspaper(path, voc)
-    filemanag.save_vocabularyAndPL_file(voc)
+        analysis.analyse_newspaper(path, tempVoc)
+    filemanag.save_vocabularyAndPL_file(tempVoc)
 
     # Applying Top K Algorithme
     postingListsOrderedById, postingListsOrderedByScore = createMockData()
 
     savedVoc = filemanag.read_vocabulary()
 
-    top_k = find_fagins_top_k(postingListsOrderedById,
-                              postingListsOrderedByScore, 3)
+    print("savedDoc : {}".format(savedVoc))
+
+    print(query.get_posting_list(savedVoc,"aa", filemanag))
+    apply_top_k_algo(['aa','bb'],savedVoc,filemanag,1)
+
+    # top_k = find_fagins_top_k(postingListsOrderedById,
+    #                           postingListsOrderedByScore, 3)
