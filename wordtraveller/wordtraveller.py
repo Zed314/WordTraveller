511808a5dd1e8581477f04d63393d2fe26e505ca
Alex Huang Feng <alexhuang05@gmail.com>
2018-11-08 23:04:00 +0100

    indent

--- wordtraveller/wordtraveller.py
+++ wordtraveller/wordtraveller.py
@@ -9,21 +9,21 @@ from . import view
 def analysis_parameters():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-d", type=str, default = './workspace/',
+    parser.add_argument("-d", type=str, default='./workspace/',
                         help="dossier avec les fichier VOC et PL résultat de l'indexation")
     parser.add_argument("-f", type=str,
                         help="nom de fichier VOC et PL ", required=True)
     parser.add_argument("-q", type=str,
-                        help="requête des termes separés par un virgule. Ex: voiture,maison ", required = True)
+                        help="requête des termes separés par un virgule. Ex: voiture,maison ", required=True)
     parser.add_argument("--file", type=str,
                         help="fichier avec un nombre de mots pour la requête (à definir la structure de ce fichier)")
-    parser.add_argument("-n", type=int, default = 3,
+    parser.add_argument("-n", type=int, default=3,
                         help="nombre de résultats souhaité de documents ")
-    parser.add_argument("--algo", type=str,default="naive",
+    parser.add_argument("--algo", type=str, default="naive",
                         help="algorithme souhaité pour l'indexation ")
-    parser.add_argument("--view", type=str,default="simple",
+    parser.add_argument("--view", type=str, default="simple",
                         help="type de visulasation simple ou fullText ")
-    parser.add_argument("--vpath", type=str,default="./data/latimesMini/",
+    parser.add_argument("--vpath", type=str, default="./data/latimesMini/",
                         help="path des fichier sources pour --view fullText")
 
     args = parser.parse_args()
@@ -31,16 +31,16 @@ def analysis_parameters():
     filemanager = fm.FileManager(args.f, args.d)
     savedVoc = filemanager.read_vocabulary()
 
-    switchAlgo = {"naive":naivetopk.naive_top_k_algo,
-    "fagins": faginstopk.apply_top_k_algo,
-    "faginsTA": faginsta.apply_fagins_ta}
-    
+    switchAlgo = {"naive": naivetopk.naive_top_k_algo,
+                  "fagins": faginstopk.apply_top_k_algo,
+                  "faginsTA": faginsta.apply_fagins_ta}
+
     algoFunct = switchAlgo[args.algo]
     words = args.q.split(",")
     result = algoFunct(words, savedVoc, filemanager, args.n)
 
     switchView = {"simple": view.displayResults,
-    "fullText": view.displayResultsText}
+                  "fullText": view.displayResultsText}
     viewFunct = switchView[args.view]
     viewFunct(result, args.vpath)
 
