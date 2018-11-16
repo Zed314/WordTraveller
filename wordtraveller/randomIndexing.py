c2a5ce8fc440d0ddd568e7297af3facd6378b559
Alex Huang Feng <alexhuang05@gmail.com>
2018-11-16 23:00:49 +0100

    random Indexing class

--- wordtraveller/randomIndexing.py
+++ wordtraveller/randomIndexing.py
@@ -3,67 +3,94 @@ from sortedcontainers import SortedDict
 import numpy as np
 from pathlib import Path
 
-preprocessor = preprocessing.Preprocessor()
+# TODO: pour saving to disk
+import struct
+import os
 
-def randomIndexing(path, nbDocToStart = 0, nbDocToScan = -1):
-    # if(not path.endswith('/')):
-    #     path = path + '/'
-    DOCUMENT_DIMENSION = 20
+
+class RandomIndexing:
+    DOCUMENT_DIMENSION = 15
     TERM_DIMENSION = DOCUMENT_DIMENSION
+    NON_ZEROS_START = 3
+    NON_ZEROS_END = 8
+    ENCODING = ''
+
+    preprocessor = preprocessing.Preprocessor()
+
+    def __init__(self, preprocessorToSet):
+        if(preprocessorToSet):
+            self.preprocessor = preprocessorToSet
+        self.documents = {}
+        self.voc_doc = {}
+
+    def setDimensions(self, doc_dim, term_dim, non_z_start, non_z_end):
+        self.DOCUMENT_DIMENSION = doc_dim
+        self.TERM_DIMENSION = term_dim
+        self.NON_ZEROS_START = non_z_start
+        self.NON_ZEROS_END = non_z_end
+
+    def setDocumentVector(self, docId):
+        self.documents[docId] = np.zeros((self.DOCUMENT_DIMENSION,), dtype=int)
+        non_zeros = np.random.random_integers(self.NON_ZEROS_START, self.NON_ZEROS_END)
+        for x in range(0, non_zeros):
+            index = np.random.random_integers(self.DOCUMENT_DIMENSION-1,)
+            if index % 2:
+                self.documents[docId][index] = -1
+            else:
+                self.documents[docId][index] = 1
+        print("documents[{}] {}".format(docId, self.documents[docId]))
+
+    def addTermVector(self, term, docId):
+        if term in self.voc_doc:
+            self.voc_doc[term] = self.voc_doc[term] + self.documents[docId]
+        else:
+            self.voc_doc[term] = np.zeros((self.TERM_DIMENSION,), dtype=int)
+        print("voc[{}]: {}".format(term, self.voc_doc[term]))
+
+    def getTermsVectors(self):
+        return self.voc_doc
+
+def randomIndexing(path, documents, voc_doc, nbDocToStart=0, nbDocToScan=-1):
+
+    global DOCUMENT_DIMENSION
+    global TERM_DIMENSION
+    global NON_ZEROS_START, NON_ZEROS_END
 
     file = open(path, "r")
-    
-    voc = SortedDict()
-    
+
     currentDocId = 0
 
     isInText = False
     isInParagraph = False
-    # currDoc = 0
     nbDocScanned = 0
     documentText = ""
-    documents = {}
     for line in file:
         if line.startswith("<DOCID>"):
             currentDocId = int(line[len("<DOCID> "):-len(" </DOCID>\n")])
-            documents[currentDocId] = np.zeros((DOCUMENT_DIMENSION,), dtype=int)
-            non_zeros = np.random.random_integers(3,8)
+            documents[currentDocId] = np.zeros(
+                (DOCUMENT_DIMENSION,), dtype=int)
+            non_zeros = np.random.random_integers(
+                NON_ZEROS_START, NON_ZEROS_END)
             for x in range(0, non_zeros):
                 index = np.random.random_integers(DOCUMENT_DIMENSION-1,)
-                if index%2:
+                if index % 2:
                     documents[currentDocId][index] = -1
                 else:
                     documents[currentDocId][index] = 1
-            print("documents[{}] {}".format(currentDocId,documents[currentDocId]))
+            print("documents[{}] {}".format(
+                currentDocId, documents[currentDocId]))
 
         elif line.startswith("</DOC>"):
-            # We use the data we accumulate during the process
-            # if nbDocToStart > currDoc:
-            #     voc_doc = {}
-            #     documentText = ""
-            #     currDoc += 1
-            #     continue
-            # if nbDocToScan == currDoc:
-            #     break
-            voc_doc = {}
             terms = preprocessor.process(documentText)
-            # terms.append("***NumberDifferentDocs***")
             for term in terms:
                 if term in voc_doc:
                     voc_doc[term] = voc_doc[term] + documents[currentDocId]
                 else:
-                    voc_doc[term] = [0]*TERM_DIMENSION
-                print("voc[{}]: {}".format(term,voc_doc[term]))
-            # for term, occurrences in voc_doc.items():
-            #     if term in voc:
-            #         voc[term][currentDocId] = [0, occurrences]
-            #     else:
-            #         voc[term] = SortedDict()
-            #         voc[term][currentDocId] = [0, occurrences]
+                    voc_doc[term] = np.zeros((TERM_DIMENSION,), dtype=int)
+                print("voc[{}]: {}".format(term, voc_doc[term]))
             documentText = ""
-            # currDoc += 1
             nbDocScanned += 1
-            if nbDocScanned == nbDocToScan and nbDocToScan !=-1:
+            if nbDocScanned == nbDocToScan and nbDocToScan != -1:
                 break
         elif line.startswith("<TEXT>"):
             isInText = True
@@ -76,14 +103,39 @@ def randomIndexing(path, nbDocToStart = 0, nbDocToScan = -1):
         elif line.startswith("<"):
             pass
         elif isInText and isInParagraph:
-            # if nbDocToStart > currDoc:
-            #     continue
             documentText += line
     file.close()
 
 
-if __name__ == "__main__":
-    pathlist = Path('./tests/data/test4/').glob('**/la*')
-    for path in pathlist:
-        randomIndexing(path)
+def saveSemantics(vocabulary, path_to_write):
+    print("VOC: {}".format(vocabulary))
 
+    if not os.path.exists(path_to_write):
+        os.makedirs(path_to_write)
+    # create the files
+    if not os.path.isfile(self.getPathVoc()):
+        file = open(self.getPathVoc(), "wb")
+        file.close()
+    if not os.path.isfile(self.getPathPL()):
+        file = open(self.getPathPL(), "wb")
+        file.close()
+    file = open(path_to_write + 'termvect.vect', "w")
+    for voc in vocabulary:
+        file.write("{},{}\n".format(voc, vocabulary[voc]))
+    file.close()
+    # np_list = np.array([1,2,3,4,5,6,7,8,9,10])
+    # buf = struct.pack('10i',*np_list)
+    # uncoded = struct.unpack('10i',buf)
+
+    pass
+
+
+if __name__ == "__main__":
+    pathlist = Path('./tests/data/test3/').glob('**/la*')
+    documents = {}
+    voc_doc = {}
+
+    for path in pathlist:
+        randomIndexing(path, documents, voc_doc)
+
+    saveSemantics(voc_doc, './workspace/proves/')
