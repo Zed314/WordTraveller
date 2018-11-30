120941278647ad3b123c7b813ad23e7e2a699ea4
Alex Huang Feng <alexhuang05@gmail.com>
2018-11-30 19:44:28 +0100

    TODOs out

--- wordtraveller/query.py
+++ wordtraveller/query.py
@@ -16,12 +16,3 @@ def get_posting_list(voc, word, fileManager, returnPostingListOrderedByScore = F
             return SortedDict(), SortedDict()
         else:
             return SortedDict()
-
-
-
-if __name__ == "__main__" :
-
-    # TODO: cacher le filemanager, dés l'exterior, on ne devrait que acceder à analysis et a query
-    fileManager = filemanager.FileManager("test1","./workspace/")
-    voc = fileManager.read_vocabulary()
-    print(get_posting_list(voc,"bb", fileManager ))
